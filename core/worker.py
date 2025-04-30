2d0460cf92ca9d33345304dca1f68b3fa4ea5c7b
Galactic647 <44773161+Galactic647@users.noreply.github.com>
2025-04-30 11:34:39 +0700

    Add worker for persistent cdp connection

--- core/worker.py
+++ core/worker.py
@@ -1,6 +1,7 @@
 from PySide2.QtCore import QObject, Signal
 
 import traceback
+import asyncio
 
 
 class GenericWorker(QObject):
@@ -20,3 +21,26 @@ class GenericWorker(QObject):
         except Exception:
             self.result.emit(traceback.format_exc())
         self.finished.emit()
+
+
+class AsyncRunnerWorker(QObject):
+    result = Signal(object)
+    finished = Signal()
+
+    def __init__(self, coro, *args, **kwargs):
+        super().__init__()
+        self.coro = coro
+        self.args = args
+        self.kwargs = kwargs
+        
+        self.loop = None
+
+    def run(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+        result = self.loop.run_until_complete(self.coro(*self.args, **self.kwargs))
+        self.result.emit(result)
+        self.finished.emit()
+
+        self.loop.close()
